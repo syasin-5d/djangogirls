8ba3a8895dc6a1b99f97ed9a2ee52300c35fd790
syasin <syasinahya@gmail.com>
2018-05-26 02:00:07 +0900

    create virtualenv

---
+++ djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/syasin/src/github.com/syasin-5d/djangogirls/djangogirls/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
